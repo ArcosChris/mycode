bfebf48e439d99286627a18ca52c813db65412dc
ArcosChris <arcoschris456@gmail.com>
2023-01-03 23:53:12 +0000

    added challenges directory

---
+++ challenges/challenge-18.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+def main():
+    user_name = input("What is your name?")
+    day_of_week = input("What is the day of the week?")
+    print("Hello, " + user_name + "! Happy " + day_of_week + "!")
+
+
+main()
