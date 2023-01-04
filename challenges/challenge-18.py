b865c9c8dcbe9fd02826074eca092e560764d60b
ArcosChris <arcoschris456@gmail.com>
2023-01-04 21:29:00 +0000

    string join and split

--- challenges/challenge-18.py
+++ challenges/challenge-18.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
+'''Carcos | Christopher.arcos@tlgcohort.com
+   Challenge 18
+'''
 
 def main():
     user_name = input("What is your name?")
     day_of_week = input("What is the day of the week?")
-    print("Hello, " + user_name + "! Happy " + day_of_week + "!")
-
+    print(f"Hello, {user_name}! Happy {day_of_week}!")
 
 main()
---
+++ strings/stringmethod01.py
@@ -0,0 +1,17 @@
+#!usr/bin/env python3
+"""
+Carcos | christopher.arcos@tlgcohort.com | Learning about python strings
+"""
+
+# create a small string
+lilstring = "Alta3 Research offers classes on Python coding"
+newlist = lilstring.split(" ") # this returns a list
+print(newlist)
+
+# create a list of strings
+myiplist = ["192", "168", "0", "12"]
+# set singleip as the result of joining the list myiplist around the "."
+singleip = ".".join(myiplist)
+# display singleip
+print(singleip)
+
