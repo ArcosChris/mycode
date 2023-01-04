dfacd3f0e2c01ecd0d35c75d150c7492f27d7439
ArcosChris <arcoschris456@gmail.com>
2023-01-04 21:51:07 +0000

    new challenge

---
+++ challenges/tuesdaychallenge.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Carcos | christopher.arcos@tlgcohort.com | Challenge Exercise
+"""
+
+mylist = [1,2,3,4,5, "Python"]
+
+name = input("What is your name?\n")
+
+# This is what you should see when print runs-
+# Hi <name>! Welcome to Day 2 of Python Training!
+print(f"Hi {name.upper()}! Welcome to Day {mylist[1]} of {mylist[5]} Training!")
