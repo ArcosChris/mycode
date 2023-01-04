cf245bf80d9944aaace8acd0bc5bb08c951bf976
ArcosChris <arcoschris456@gmail.com>
2023-01-04 22:01:44 +0000

    added new challeneg

---
+++ mix-list/mixlist01.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+my_list = [ "192.168.0.5", 5060, "UP" ]
+
+#print the item at index 0 in the list
+print(f"The first item in the list (IP): {my_list[0]}")
+
+#display the item at index 1 and convert it to string 
+print(f"The second item in the list (port): {my_list[1]}")
+
+#print the last item at index 2 in list
+print(f"The last item in the list (state): {my_list[2]}")
+
+iplist = [ 5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh" ]
+
+print(f"IP addresses: {iplist[3]}, {iplist[4]}")
+
