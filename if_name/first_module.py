031e9fc7f62e84e50ddd293164152515b7b017fc
ArcosChris <arcoschris456@gmail.com>
2023-01-06 20:37:39 +0000

    practice with if name and imports

---
+++ if_name/first_module.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+CARCOS | christopher.arcos@tlgcohort.com | If__name
+"""
+
+print("this code will alway execute")
+
+def main():
+    print("This code belongs to the main function in module 1")
+
+
+if __name__ == "__main__":
+    main()
---
+++ if_name/second_module.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+CARCOS | Christopher.arcos@tlgcohort.com | Working with modules (importing)
+"""
+
+#importing the first module created by us 
+import first_module
+
+first_module.main() #calling the main function in the first_module
+
+print("Module #2 Name =", __name__)
+
