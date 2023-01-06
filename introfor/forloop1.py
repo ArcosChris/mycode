c9b802c2592206c0afc7d31f24010cfb14a37bc6
ArcosChris <arcoschris456@gmail.com>
2023-01-06 21:37:26 +0000

    added for exercises

---
+++ introfor/forloop1.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Carcos | christopher.arcos@tlgcohort.com | Working with for
+"""
+
+
+#creating a list of vendors
+vendors = ["cisco", "juniper", "big_ip", "f5", "arista"]
+
+#loop thorugh vendors
+
+for vendor in vendors:
+    print(f"The vendor is : {vendor}") # each vendor will be printed
+
+print("Loop is done now (: ")
+
