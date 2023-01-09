5cbfd2c338f58312bf5761406dc25d88b27de57a
ArcosChris <arcoschris456@gmail.com>
2023-01-09 18:44:11 +0000

    added new structure for dir

---
+++ week2/lrnimport/intscript.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+from subprocess import call
+
+call(["ip","link", "show", "up"])
+
+print("This program will check your interfaces.")
+
+
+interface = input("Enter an interface, like, ens3: ")
+
+call(["ip", "addr", "show", "dev", interface])
+
+
---
+++ week2/pylint/smallscript.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""
+Author: Carcos
+
+This program prints some things to the screen
+"""
+
+def main():
+    """Our main program"""
+    mynote = 'print this string' # define mynote
+    print(mynote)                # display our string
+
+if __name__ == "__main__":
+    main()
