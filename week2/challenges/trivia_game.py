4be742b3d9b97e56ff0df2d23732890930a5b193
Christopher Arcos <arcoschris456@gmail.com>
2023-01-12 09:16:35 -0800

    added the trivia game changes

--- week2/challenges/trivia_game.py
+++ week2/challenges/trivia_game.py
@@ -4,6 +4,7 @@
 import html
 import requests
 import random
+import crayons
 
 def user_choices():
     categories = {
@@ -40,28 +41,56 @@ def user_choices():
         print(f"{k} - {v}")
 
     category = input("\nPlease enter a category code:\n")
-    difficulty = input("\nPlease enter a difficulty level\nEasy\nMedium\nHard\n").lower()
+    difficulty = input("\nPlease enter a difficulty level (Easy | Medium | Hard)\n").lower()
     custom_api_url = (f"{base}amount={amount}&category={category}&type=multiple&difficulty={difficulty}")
     start_game(custom_api_url)
 
 def start_game(api):
     resp = requests.get(api).json()
     cat = resp['results'][0].get('category').title()
-    print('=======================')
+    print('\n=======================')
     print(f"{cat}")
     print('=======================')
     
     for question in resp['results']:
-        answers = [{question['correct_answer']: True}]
-        print(f"{html.unescape(question['question'])}\n")
-       
-        for incorrect in question['incorrect_answers']:
-            answers.append({incorrect: False})
-        
-        random.shuffle(answers)
+        answer = html.unescape(question['correct_answer'])
+        options = [html.unescape(question['correct_answer'])]
+        options.extend(question['incorrect_answers'])
+        counter = 1
 
-        for answer in (answers):
-            print(answer)
+        print(crayons.green(f"\n{html.unescape(question['question'])}\n"))
+        random.shuffle(options)
+
+        for option in options:
+            print(crayons.blue(html.unescape(f"{counter}. {option}")))
+            counter += 1
+
+        while True:
+            try:
+                user_answer = int(input(crayons.cyan("\nPlease enter the number associated with the answer.\n")))
+                if user_answer < 1 or user_answer > len(options):
+                    print(crayons.red(f"\n==========================="))
+                    print(crayons.red("Not a valid answer. Do better."))
+                    print(crayons.red(f"===========================\n"))
+                    continue
+
+            except ValueError:
+                print(crayons.red("Please enter a valid number"))
+                continue
+            else:
+                index = user_answer - 1
+                if options[index] == answer:
+                    print(crayons.yellow(f"\n==========================="))
+                    print(crayons.yellow(f"Good Job! {answer} is correct"))
+                    print(crayons.yellow(f"===========================\n"))
+                    break
+                else:
+                    print(crayons.red(f"\n=========================================================="))
+                    print(crayons.red(f"Sorry that is not correct. The correct answer is {answer}."))
+                    print(crayons.red(f"==========================================================\n"))
+                    break
+
+
 
 def main():
     print('=======================')
